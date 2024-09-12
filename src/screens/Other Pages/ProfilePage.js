b89aa52673b278e1d16e58acfd7c447e68462c51
Imtiaz Ahmad <imtiazahmadswl93@gmail.com>
2024-09-13 01:43:46 +0500

    removed extra code

--- src/screens/Other Pages/ProfilePage.js
+++ src/screens/Other Pages/ProfilePage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Profile from '../../components/Other Pages/Profile Page/Profile';
-import PaymentsMethod from '../../components/Other Pages/Profile Page/PaymentsMethod'
 import ProfileSetting from '../../components/Other Pages/Profile Page/ProfileSetting'
 import AuthenticationDetail from '../../components/Other Pages/Profile Page/AuthenticationDetail'
 import PageHeader1 from '../../components/common/PageHeader1';
@@ -13,7 +12,6 @@ function ProfilePage() {
             <div className='col-xl-4 col-lg-5 col-md-12'>
 
                 <Profile />
-                <PaymentsMethod />
             </div>
             <div className='col-xl-8 col-lg-7 col-md-12'>
                 <ProfileSetting />
