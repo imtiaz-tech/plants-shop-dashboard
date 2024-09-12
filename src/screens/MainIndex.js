3ef3c2b2e4c28a7de4cd3b0a6d3f4b56549cbbd7
Imtiaz Ahmad <imtiazahmadswl93@gmail.com>
2024-09-13 00:57:03 +0500

    removed extra code

--- src/screens/MainIndex.js
+++ src/screens/MainIndex.js
@@ -24,7 +24,6 @@ import ModalUI from "./Uicomponent/Modal";
 import NavbarUI from "./Uicomponent/Navbar";
 import NavsUI from "./Uicomponent/Navs";
 import PopoversUI from "./Uicomponent/Popovers";
-import SpinnersUI from "./Uicomponent/Spinners";
 import Calendar from "./App/Calendar";
 import StaterPage from "./Stater Page/StaterPage";
 import Documentation from "./Documentation/Documentation";
@@ -67,7 +66,6 @@ function MainIndex() {
           <Route path="/ui-navbarui" element={<NavbarUI />} />
           <Route path="/ui-navsui" element={<NavsUI />} />
           <Route path="/ui-popoversui" element={<PopoversUI />} />
-          <Route path="/ui-spinnersui" element={<SpinnersUI />} />
           <Route path="/stater-page" element={<StaterPage />} />
           <Route path="/documentation" element={<Documentation />} />
 
