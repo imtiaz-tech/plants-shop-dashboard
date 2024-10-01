31d32d229ed6a2282049214b24952b3bf6be9d41
Imtiaz Ahmad <imtiazahmadswl93@gmail.com>
2024-10-01 18:11:24 +0500

    updated

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,4 +1,4 @@
 import axios from "axios";
-axios.defaults.baseURL = "https://decor-plant-2jhwtyuqm-imtiaz-techs-projects.vercel.app/api/v1";
+axios.defaults.baseURL = "https://decor-plant-imtiaz-tech-imtiaz-techs-projects.vercel.app/api/v1";
 
 export default axios;
